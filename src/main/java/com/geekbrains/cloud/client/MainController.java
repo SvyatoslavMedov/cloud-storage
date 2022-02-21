7341bee530327c684bb3a2f576b852c7941511fd
SvyatoslavMedov <irobot1204@gmail.com>
2022-02-21 21:09:20 +0600

    make client-cloud

---
+++ src/main/java/com/geekbrains/cloud/client/MainController.java
@@ -0,0 +1,23 @@
+package com.geekbrains.cloud.client;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class MainController {
+
+    public TextField clientPath;
+    public TextField serverPath;
+    public ChoiceBox <String> disks;
+    public ListView <String> clientView;
+    public ListView <String> serverView;
+
+    public void download(ActionEvent actionEvent) {
+    }
+
+    public void upload(ActionEvent actionEvent) {
+    }
+
+
+}
