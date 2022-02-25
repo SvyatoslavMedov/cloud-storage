4821dda78e3c31bf7b01de31919f7858f7c4a668
SvyatoslavMedov <irobot1204@gmail.com>
2022-02-25 22:04:32 +0600

    make CloudClientApplication

--- src/main/java/com/geekbrains/cloud/client/MainController.java
+++ src/main/java/com/geekbrains/cloud/client/MainController.java
@@ -1,17 +1,37 @@
 package com.geekbrains.cloud.client;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-public class MainController {
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+
+    private File currentDirectory;
 
     public TextField clientPath;
     public TextField serverPath;
     public ChoiceBox <String> disks;
     public ListView <String> clientView;
     public ListView <String> serverView;
+
+
+    //Platform.runLater(() -> {})
+    private void updateClientView(){
+
+        clientView.getItems().clear();
+        clientView.getItems().add("...");
+        clientView.getItems()
+                .addAll(currentDirectory.list());
+
+    }
 
     public void download(ActionEvent actionEvent) {
     }
@@ -20,4 +40,10 @@ public class MainController {
     }
 
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        currentDirectory = new File(System.getProperty("user.home"));
+        Platform.runLater(this::updateClientView);
+
+    }
 }
