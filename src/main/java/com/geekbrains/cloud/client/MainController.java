c41cf542cbed7f420bdb4223e0a0fa352dd63e99
SvyatoslavMedov <irobot1204@gmail.com>
2022-03-02 20:56:56 +0600

    make CloudClientApplication

--- src/main/java/com/geekbrains/cloud/client/MainController.java
+++ src/main/java/com/geekbrains/cloud/client/MainController.java
@@ -3,34 +3,37 @@ package com.geekbrains.cloud.client;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
 
 
-    private File currentDirectory;
-
     public TextField clientPath;
     public TextField serverPath;
-    public ChoiceBox <String> disks;
     public ListView <String> clientView;
     public ListView <String> serverView;
+    private File currentDirectory;
 
+    private DataInputStream is;
+    private DataOutputStream os;
 
     //Platform.runLater(() -> {})
     private void updateClientView(){
-
-        clientView.getItems().clear();
-        clientView.getItems().add("...");
-        clientView.getItems()
-                .addAll(currentDirectory.list());
-
+        Platform.runLater(()-> {
+            clientPath.setText(currentDirectory.getAbsolutePath());
+            clientView.getItems().clear();
+            clientView.getItems().add("...");
+            clientView.getItems()
+                    .addAll(currentDirectory.list());
+        });
     }
 
     public void download(ActionEvent actionEvent) {
@@ -39,11 +42,39 @@ public class MainController implements Initializable {
     public void upload(ActionEvent actionEvent) {
     }
 
+    private void initNetwork() {
+        try {
+            Socket socket = new Socket("localhost",8189);
+            is = new DataInputStream(socket.getInputStream());
+            os = new DataOutputStream(socket.getOutputStream());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         currentDirectory = new File(System.getProperty("user.home"));
-        Platform.runLater(this::updateClientView);
 
+
+
+        //run in FX Thread
+        //:: - method reference
+        updateClientView();
+        clientView.setOnMouseClicked(e-> {
+            if(e.getClickCount()==2) {
+                String item = clientView.getSelectionModel().getSelectedItem();
+                if (item.equals("...")){
+                    currentDirectory = currentDirectory.getParentFile();
+                    updateClientView();
+                }else{
+                    File selected = currentDirectory.toPath().resolve(item).toFile();
+                    if(selected.isDirectory()){
+                        currentDirectory = selected;
+                        updateClientView();
+                    }
+                }
+            }
+        });
     }
 }
