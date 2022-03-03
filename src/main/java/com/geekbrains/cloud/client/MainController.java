48ffa17f03cb425361b43b3fe095222a74f98c7e
SvyatoslavMedov <irobot1204@gmail.com>
2022-03-03 21:44:31 +0600

    make CloudClientApplication

--- src/main/java/com/geekbrains/cloud/client/MainController.java
+++ src/main/java/com/geekbrains/cloud/client/MainController.java
@@ -6,24 +6,29 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
+import java.io.*;
 import java.net.Socket;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+
+    private static final int BUFFER_SIZE = 8192;
 
 
     public TextField clientPath;
     public TextField serverPath;
     public ListView <String> clientView;
+
+    // данных нет на клиенте и не может быть!!!
     public ListView <String> serverView;
     private File currentDirectory;
 
     private DataInputStream is;
     private DataOutputStream os;
+    private byte[] buf;
 
     //Platform.runLater(() -> {})
     private void updateClientView(){
@@ -36,21 +41,100 @@ public class MainController implements Initializable {
         });
     }
 
-    public void download(ActionEvent actionEvent) {
+    private void updateServerView(List<String> names)  {
+        Platform.runLater(()-> {
+
+            serverView.getItems().clear();
+            serverView.getItems().add("...");
+            serverView.getItems()
+                    .addAll(names);
+        });
+
     }
 
-    public void upload(ActionEvent actionEvent) {
+    public void download(ActionEvent actionEvent) throws IOException {
+        os.writeUTF("#get_file#");
+        os.writeUTF(serverView.getSelectionModel().getSelectedItem());
+        os.flush();
+    }
+
+
+    //upload file to server
+    public void upload(ActionEvent actionEvent) throws IOException {
+        String item = clientView.getSelectionModel().getSelectedItem();
+        File selected = currentDirectory.toPath().resolve(item).toFile();
+        if(selected.isFile()){
+            os.writeUTF("#file_message#");
+            os.writeUTF(selected.getName());
+            os.writeLong(selected.length());
+            try(InputStream fis = new FileInputStream(selected)){
+                while(fis.available() > 0) {
+                    int readBytes = fis.read(buf);
+                    os.write(buf,0,readBytes);
+                }
+            }
+            os.flush();
+        }
     }
 
     private void initNetwork() {
         try {
-            Socket socket = new Socket("localhost",8189);
+            buf = new byte[BUFFER_SIZE];
+            Socket socket = new Socket("localhost",8192);
             is = new DataInputStream(socket.getInputStream());
             os = new DataOutputStream(socket.getOutputStream());
+            Thread readThread = new Thread(this :: readFromInputStream);
+            readThread.setDaemon(true);
+            readThread.start();
+
         }catch(Exception e){
             e.printStackTrace();
         }
     }
+
+
+    private void readFromInputStream() {
+        try {
+            while (true) {
+                String command = is.readUTF();
+                // #List# size name1, name2 ... name_size
+                // #path# pathName
+                // #path_action# dirName
+                if ("#list".equals(command)) {
+                    List<String> names = new ArrayList<>();
+                    int count = is.readInt();
+                    for (int i = 0; i < count; i++) {
+                        String name = is.readUTF();
+                        names.add(name);
+                    }
+                    updateServerView(names);
+                }
+                if ("#path#".equals(command)) {
+                    String serverDir = is.readUTF();
+                    Platform.runLater(() -> serverPath.setText(serverDir));
+                }
+                if ("#file_message#".equals(command)) {
+
+                    String name = is.readUTF();
+                    long size = is.readLong();
+                    File newFile = currentDirectory.toPath()
+                            .resolve(name)
+                            .toFile();
+                    try (OutputStream fos = new FileOutputStream(newFile)) {
+                        for (int i = 0; i < (size + BUFFER_SIZE - 1) / BUFFER_SIZE; i++) {
+                            int readCount = is.read(buf);
+                            fos.write(buf, 0, readCount);
+                        }
+                    }
+                updateClientView();
+                }
+            }
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -61,6 +145,10 @@ public class MainController implements Initializable {
         //run in FX Thread
         //:: - method reference
         updateClientView();
+        initNetwork();
+
+
+
         clientView.setOnMouseClicked(e-> {
             if(e.getClickCount()==2) {
                 String item = clientView.getSelectionModel().getSelectedItem();
---
+++ src/main/java/com/geekbrains/cloud/server/CloudFileHandler.java
@@ -0,0 +1,96 @@
+package com.geekbrains.cloud.server;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CloudFileHandler implements Runnable {
+
+
+    private static final int BUFFER_SIZE = 8192;
+    private final Socket socket;
+    private final DataInputStream is;
+    private final DataOutputStream os;
+    private byte[] buf;
+    private File serverDirectory;
+
+    public CloudFileHandler(Socket socket) throws IOException {
+        this.socket = socket;
+        System.out.println("Client connected!");
+        is = new DataInputStream(socket.getInputStream());
+        os = new DataOutputStream(socket.getOutputStream());
+        buf = new byte[BUFFER_SIZE];
+        serverDirectory = new File ("server");
+        sendServerFiles();
+        sendCurrentDirName();
+    }
+
+    private List<String> getServerFiles(){
+        String[] names = serverDirectory.list();
+        if (names != null) {
+            return Arrays.asList(serverDirectory.list());
+        }
+        return new ArrayList<>();
+    }
+
+    private void sendCurrentDirName() throws IOException {
+        os.writeUTF("#path#");
+        os.writeUTF(serverDirectory.getName());
+
+    }
+
+    private void sendServerFiles() throws IOException{
+        //#List# size name1, name2 ... name_size
+        os.writeUTF("#list");
+        List<String> files = getServerFiles();
+        os.writeInt(files.size());
+        for (String file : files) {
+            os.writeUTF(file);
+
+        }
+
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true){
+                String command = is.readUTF();
+                if("#file_message#".equals(command)){
+                    String name = is.readUTF();
+                    long size = is.readLong();
+                    File newFile = serverDirectory.toPath().resolve(name).toFile();
+                    try(OutputStream fos = new FileOutputStream(newFile)) {
+                        for (int i = 0; i < (size + BUFFER_SIZE - 1) / BUFFER_SIZE; i++) {
+                            int readCount = is.read(buf);
+                            fos.write(buf, 0, readCount);
+                        }
+                    }
+                    System.out.println("File: " + name + " was uploaded");
+                    sendServerFiles();
+                }else if ("#get_file#".equals(command)) {
+                    String fileName = is.readUTF();
+                    File file = serverDirectory.toPath().resolve(fileName).toFile();
+                    os.writeUTF("#file_message#");
+                    os.writeUTF(fileName);
+                    os.writeLong(file.length());
+                    try(InputStream fis = new FileInputStream(file)) {
+                        while ((fis.available() > 0)){
+                            int count = fis.read(buf);
+                            os.write(buf,0,count);
+                        }
+                    }
+                    os.flush();
+                }
+                else{
+                    System.err.println("Unknown command: " + command);
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
