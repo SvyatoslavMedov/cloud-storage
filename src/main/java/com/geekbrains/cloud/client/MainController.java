21b06ae88f29f28413e4a207739a68ac9b836499
SvyatoslavMedov <irobot1204@gmail.com>
2022-03-03 21:44:31 +0600

    make CloudClientApplication

--- src/main/java/com/geekbrains/cloud/client/MainController.java
+++ src/main/java/com/geekbrains/cloud/client/MainController.java
@@ -6,14 +6,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
+import java.io.*;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+
+    private static final int BUFFER_SIZE = 256;
 
 
     public TextField clientPath;
@@ -24,6 +24,7 @@ public class MainController implements Initializable {
 
     private DataInputStream is;
     private DataOutputStream os;
+    private byte[] buf;
 
     //Platform.runLater(() -> {})
     private void updateClientView(){
@@ -37,13 +38,31 @@ public class MainController implements Initializable {
     }
 
     public void download(ActionEvent actionEvent) {
+
     }
 
-    public void upload(ActionEvent actionEvent) {
+
+    //upload file to server
+    public void upload(ActionEvent actionEvent) throws IOException {
+        String item = clientView.getSelectionModel().getSelectedItem();
+        File selected = currentDirectory.toPath().resolve(item).toFile();
+        if(selected.isFile()){
+            os.writeUTF("#file_message#");
+            os.writeUTF(selected.getName());
+            os.writeLong(selected.length());
+            try(InputStream fis = new FileInputStream(selected)){
+                while(fis.available() > 0) {
+                    int readBytes = fis.read(buf);
+                    os.write(buf,0,readBytes);
+                }
+            }
+            os.flush();
+        }
     }
 
     private void initNetwork() {
         try {
+            buf = new byte[BUFFER_SIZE];
             Socket socket = new Socket("localhost",8189);
             is = new DataInputStream(socket.getInputStream());
             os = new DataOutputStream(socket.getOutputStream());
@@ -61,6 +80,7 @@ public class MainController implements Initializable {
         //run in FX Thread
         //:: - method reference
         updateClientView();
+        initNetwork();
         clientView.setOnMouseClicked(e-> {
             if(e.getClickCount()==2) {
                 String item = clientView.getSelectionModel().getSelectedItem();
